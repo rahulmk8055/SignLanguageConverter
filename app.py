6124f4d7256d284e0459574a3254b93a408ce9d8
rahul <rahulmk8055@gmail.com>
2024-04-14 18:27:55 -0400

    confidence score check

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import streamlit as st
+from mediapipe.python.solutions.drawing_utils import DrawingSpec
+
 from state import State
 from time import time
 from streamlit_pills import pills
@@ -13,6 +15,7 @@ state = State()
 state.load_state()
 
 confidence = 0
+predicted_character = ""
 
 
 def write_value_to_file(value):
@@ -31,10 +34,9 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3, max_num_hands=1)
 
-labels_dict = {0: 'M', 1: 'N', 2: 'P', 3: 'Q', 4: 'R'}
 detection_start_time = None
 last_detected_character = ""
-hold_time = 2  # seconds
+hold_time = 2.5  # seconds
 
 st.set_page_config(page_title="ASL", layout='wide')
 
@@ -49,10 +51,11 @@ st.markdown("""
         </style>
         """, unsafe_allow_html=True)
 
-st.markdown("<h2 style='text-align: center;'>ASL Interpretation Tool</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>Sign Language Interpretation and Learning Tool</h1>",
+            unsafe_allow_html=True)
 st.markdown('#')
 
-value = ""
+value = "clear"
 
 
 def clear_word():
@@ -64,13 +67,14 @@ with st.container():
     with st.container(border=True):
         with col1:
             videoStream = st.empty()
-            col3, col4 = st.columns([0.6, 0.4])
+            col3, col4 = st.columns([0.6, 0.4], gap="large")
             with col3:
-                value = pills("Select Letter For Sign Reference",
+                st.header("Select Letter For Sign Reference")
+                value = pills("",
                               ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+                               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'clear'])
         with col4:
-            st.image("B.png", width=250)
+            st.image("./signImages/{value}.png".format(value=value), width=300)
 
     with col2:
         predictedLetterDisplay = st.empty()
@@ -101,7 +105,22 @@ while True:
     if results.multi_hand_landmarks:
         x_, y_, data_aux = [], [], []
         for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            default_style = mp_drawing_styles.get_default_hand_connections_style()
+
+            # Create a new dictionary to store the modified style
+            modified_style = {}
+
+            # Iterate through the default style and modify the thickness
+            for connection, spec in default_style.items():
+                # Create a new DrawingSpec with modified thickness
+                modified_spec = DrawingSpec(color=(48, 255, 48), thickness=10)
+                # Assign the modified spec to the connection
+                modified_style[connection] = modified_spec
+            mp_drawing.draw_landmarks(frame,
+                                      hand_landmarks,
+                                      mp_hands.HAND_CONNECTIONS,
+                                      mp_drawing_styles.get_default_hand_landmarks_style(),
+                                      modified_spec)
 
             for landmark in hand_landmarks.landmark:
                 x_.append(landmark.x)
@@ -114,15 +133,15 @@ while True:
         prediction = model.predict([np.asarray(data_aux)])
         probabilities = model.predict_proba([np.asarray(data_aux)])
         confidence = max(max(probabilities))
+        write_value_to_file(confidence * 100)
+        predicted_character = prediction[0]
 
-        predicted_character = labels_dict[int(prediction[0])]
-        # predictedLetterDisplay.header("Predicted Letter: {letter}".format(letter=predicted_character))
+        state.set_predicted_letter(predicted_character)
         # Check if detected character is consistent
         if predicted_character == last_detected_character:
             if detection_start_time is None:
                 detection_start_time = time()
-            elif time() - detection_start_time >= hold_time:
-                state.set_predicted_letter(predicted_character)
+            elif time() - detection_start_time >= hold_time and confidence > 0.8:
                 state.append_to_current_word(predicted_character)
                 detection_start_time = None  # Reset the timer
         else:
@@ -130,30 +149,38 @@ while True:
             detection_start_time = None  # Reset the timer
     else:
         last_detected_character = ""
+        confidence = 0
+        write_value_to_file(confidence * 100)
         detection_start_time = None  # Reset the timer if no hands are detected
 
     progress_value = 0
+    colour = "green"
+    if confidence < 0.8:
+        colour = "red"
+    predictedLetterDisplay.header(
+        "Predicted Letter: :{colour}[{letter}]".format(letter=predicted_character, colour=colour))
     if detection_start_time:
         elapsed_time = time() - detection_start_time
         progress_value = int((elapsed_time / hold_time) * 100)
-        if elapsed_time < hold_time:
+        if elapsed_time < hold_time and confidence > 0.8:
             progress_bar.progress(progress_value)
-            predictedLetterDisplay.header("Predicted Letter: {letter}".format(letter=predicted_character))
-
+            # predictedLetterDisplay.header("Predicted Letter: {letter}".format(letter=predicted_character))
+            # write_value_to_file(confidence * 100)
+        elif elapsed_time < hold_time and confidence < 0.8:
+            progress_bar.progress(0)
         else:
             progress_bar.progress(100)
-            predictedLetterDisplay.header("Predicted Letter: {letter}".format(letter=predicted_character))
     else:
         progress_bar.progress(0)  # Clears the progress bar if conditions aren't met
         predictedLetterDisplay.header("Predicted Letter: ")
+        write_value_to_file(0)
 
-    wordDisplay.header("Predicted Word: :rainbow[{word}]".format(word=state.get_current_word()))
+    wordDisplay.header("Predicted Word: :green[{word}]".format(word=state.get_current_word()))
 
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame = cv2.resize(frame, (640, 360))
     videoStream.image(frame, channels="RGB", use_column_width="auto")
 
-    write_value_to_file(confidence * 100)
     cv2.waitKey(1)
 
 cap.release()
--- collect_imgs.py
+++ collect_imgs.py
@@ -3,12 +3,11 @@ import mediapipe as mp
 
 import cv2
 
-
 DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-classes = 4
+classes = ["A", "B", "C", "D"]
 dataset_size = 100
 
 mp_hands = mp.solutions.hands
@@ -16,9 +15,8 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3, max_num_hands=1)
 
-
 cap = cv2.VideoCapture(0)
-for j in range(classes):
+for j in classes:
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
 
@@ -27,17 +25,6 @@ for j in range(classes):
     done = False
     while True:
         ret, frame = cap.read()
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        results = hands.process(frame_rgb)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    frame,  # image to draw
-                    hand_landmarks,  # model output
-                    mp_hands.HAND_CONNECTIONS,  # hand connections
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
 
         cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
                     cv2.LINE_AA)
